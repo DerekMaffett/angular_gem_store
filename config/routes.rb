9cf4a8e3602275e321077912aae1e59f9baf08c5
Derek Maffett <derekgmaffett@gmail.com>
2014-10-31 13:53:55 -0700

    initialize devise for admins

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :admins
   namespace :apiv1 do
     resources :products, except: [:show, :new, :edit] do
       resources :reviews, except: [:show, :new, :edit]
--- test/models/image_test.rb
+++
@@ -1,9 +0,0 @@
-require "test_helper"
-
-describe Image do
-  let(:image) { Image.new }
-
-  it "must be valid" do
-    image.must_be :valid?
-  end
-end
